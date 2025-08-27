a0e303f568c557e6160a4dd11a9c2f0b9bd4e923
Avi Vahl <avi@vahl.co.il>
2025-08-27 22:45:05 +0300

    feat: reintroduce create-disposables package we still use it

---
+++ packages/create-disposables/README.md
@@ -0,0 +1,10 @@
+## @dazl/create-disposables
+
+[![npm version](https://badge.fury.io/js/@dazl%2Fcreate-disposables.svg)](https://badge.fury.io/js/@dazl%2Fcreate-disposables)
+[@dazl/create-disposables on GitHub](https://github.com/dazl-dev/core3-utils/tree/main/packages/create-disposables)
+
+A utility for saving objects to be disposed
+
+# License
+
+MIT
---
+++ packages/create-disposables/src/create-disposables.ts
@@ -0,0 +1,24 @@
+export function createDisposables() {
+    const disposables = new Set<() => unknown>();
+
+    return {
+        async dispose() {
+            const toDispose = Array.from(disposables).reverse();
+            disposables.clear();
+            for (const dispose of toDispose) {
+                await dispose();
+            }
+        },
+        add(disposable: Disposable) {
+            if (typeof disposable === 'function') {
+                disposables.add(disposable);
+            } else {
+                disposables.add(() => disposable.dispose());
+            }
+        },
+    };
+}
+
+export type DisposeFunction = () => unknown;
+export type Disposable = { dispose: DisposeFunction } | DisposeFunction;
+export type Disposables = ReturnType<typeof createDisposables>;
